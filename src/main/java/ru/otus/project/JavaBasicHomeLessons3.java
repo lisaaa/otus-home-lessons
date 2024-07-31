8258140c9b4c00d9b1b6f9f0cf0df97c8100688d
ruakzor <ol.v.koz@yandex.com>
2024-07-31 10:29:40 +0500

    убрала лишнюю конкатенацию

--- src/main/java/ru/otus/project/JavaBasicHomeLessons3.java
+++ src/main/java/ru/otus/project/JavaBasicHomeLessons3.java
@@ -46,7 +46,7 @@ public class JavaBasicHomeLessons3 {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
 
-                System.out.print("*" + " ");
+                System.out.print("* ");
             }
             System.out.println();
         }
